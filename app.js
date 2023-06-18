eff7515a4dab492478bbd6a9343415936428d083
kazi <kazi@localhost.localdomain>
2023-06-18 22:06:00 +0530

    fetch all product,singale product and delete product in admin done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
 
 mongoConnect(()=>{
