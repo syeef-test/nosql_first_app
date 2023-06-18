a180951f2eda99302fe6fa0574004944f2b49b77
kazi <kazi@localhost.localdomain>
2023-06-18 10:45:17 +0530

    mongodb simple data add done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -12,12 +12,25 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
- 
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
+});
+
+app.use('/admin',adminRoutes);
+
+
+mongoConnect(()=>{
+  app.listen(3000);
 });
 
--- util/database.js
+++ util/database.js
@@ -1,8 +1,34 @@
-const Sequelize = require('sequelize');
+const { result } = require('lodash');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db ;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  // MongoClient.connect('mongodb+srv://kazi:nkdhpwuqDfSvOIQc@cluster0.qeakjte.mongodb.net/?retryWrites=true&w=majority').then(client=>{
+  //   console.log("succesful connection");
+  //   _db = client.db('test');
+  //   callback();
+  // }).catch(err=>{
+  //   console.log(err);
+  //   throw err;
+  // });
+  MongoClient.connect('mongodb+srv://kazi:nkdhpwuqDfSvOIQc@cluster0.qeakjte.mongodb.net/shop?retryWrites=true&w=majority').then(client=>{
+    console.log("succesful connection");
+    _db = client.db();
+    callback();
+  }).catch(err=>{
+    console.log(err);
+    throw err;
+  });
+}
+
+const getDb = () =>{
+  if(_db){
+    return _db;
+  }
+  throw 'No Database Found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
