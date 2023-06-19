2bc875e47f438547ce0eb795bd7e1b757c44cca1
kazi <kazi@localhost.localdomain>
2023-06-19 21:06:49 +0530

    cart function added

--- app.js
+++ app.js
@@ -21,17 +21,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('648fe1d7266fc48903f777ad')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
 });
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
-mongoConnect(()=>{
+mongoConnect(() => {
   app.listen(3000);
 });
 
