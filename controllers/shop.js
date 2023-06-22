40557ed4d455402a6231011e127c0e2b1798a984
syeefislam <syeefislam@yahoo.com>
2023-06-22 21:27:22 +0530

    cart display done

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,17 +44,17 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    // .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
         products: products
       });
-    }).catch(err => console.log(err));
-
-
-
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
