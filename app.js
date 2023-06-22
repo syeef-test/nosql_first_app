4842df2efa146273f62015eab98c50293f812219
syeefislam <syeefislam@yahoo.com>
2023-06-22 20:15:44 +0530

    user and product relation created

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('64945727ad9d3c75731082e8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,19 @@ mongoose
     'mongodb+srv://kazi:nkdhpwuqDfSvOIQc@cluster0.qeakjte.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
+    User.findOne().then(user=>{
+      if(!user){
+        const user = new User({
+          name:'kazi',
+          email:'kazi@gmail.com',
+          cart:{
+            items:[]
+          }
+        });
+        user.save();
+      }
+    });
+    
     app.listen(3000);
   })
   .catch(err => {
